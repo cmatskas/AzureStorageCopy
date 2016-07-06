474ac8a914eac82def53401c488e9dd2a6ae4e79
Christos Matskas <chmatsk@microsoft.com>
2016-07-06 19:16:01 +0100

    first commit

---
+++ AzStorageCopy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.DataMovement;
+using System.Threading.Tasks;
+using AzureCopyUtil;
+
+namespace AzStorageCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO : Replace all console logging with LibLog
+            var p = new Program();
+            var blobUrlToCopy = p.GetQueueItem().GetAwaiter().GetResult();
+            p.CopyBlob(blobUrlToCopy, "cm-test-container").GetAwaiter().GetResult();
+        }
+
+        public async Task<string> GetQueueItem()
+        {
+            //TODO: Add code to retrieve code from Azure Storage Queue
+            return "hello world";
+        }
+
+        public async Task CopyBlob(string sourceBlobUri, string destinationContainer)
+        {
+            var destinationBlobName = AzureUtil.GetBlobNameFromUri(sourceBlobUri);
+            var destinationBlob = AzureUtil.GetCloudBlob(destinationContainer, destinationBlobName, BlobType.BlockBlob);
+                     
+            //TODO: Add Timer for statistics
+
+            try
+            {
+                await TransferManager.CopyAsync(new Uri(sourceBlobUri), destinationBlob, false);
+            }
+            catch (Exception e)
+            {
+                //Log error to separate file to ensure that uncopied files can be transferred at a later point
+                Console.WriteLine(e.Message);
+            }
+        
+            Console.WriteLine("CloudBlob {0} is copied to {1} successfully.", sourceBlob.Uri.ToString(), destinationBlob.Uri.ToString());
+        }
+    }
+}
---
+++ AzureCopyUtil/AzureUtil.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.File;
+
+namespace AzureCopyUtil
+{
+    /// <summary>
+    /// A helper class provides convenient operations against storage account configured in the App.config.
+    /// </summary>
+    public class AzureUtil
+    {
+        private static CloudStorageAccount storageAccount;
+        private static CloudBlobClient blobClient;
+        private static CloudFileClient fileClient;
+
+        public static CloudBlob GetCloudBlob(string containerName, string blobName, BlobType blobType)
+        {
+            CloudBlobClient client = GetCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference(containerName);
+            container.CreateIfNotExists();
+
+            CloudBlob cloudBlob;
+            switch (blobType)
+            {
+                case BlobType.AppendBlob:
+                    cloudBlob = container.GetAppendBlobReference(blobName);
+                    break;
+                case BlobType.BlockBlob:
+                    cloudBlob = container.GetBlockBlobReference(blobName);
+                    break;
+                case BlobType.PageBlob:
+                    cloudBlob = container.GetPageBlobReference(blobName);
+                    break;
+                case BlobType.Unspecified:
+                default:
+                    throw new ArgumentException(string.Format("Invalid blob type {0}", blobType.ToString()), "blobType");
+            }
+
+            return cloudBlob;
+        }
+
+        public static ICloudBlob GetCloubBlob(string blobUri)
+        {
+            var client = GetCloudBlobClient();
+            return client.GetBlobReferenceFromServer(new Uri(blobUri));
+        }
+
+        public static CloudBlobDirectory GetCloudBlobDirectory(string containerName, string directoryName)
+        {
+            CloudBlobClient client = GetCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference(containerName);
+            container.CreateIfNotExists();
+
+            return container.GetDirectoryReference(directoryName);
+        }
+
+        public static string GetBlobNameFromUri(string blobUri)
+        {
+            return blobUri.Split(@'/').Last();
+        }
+
+        private static CloudBlobClient GetCloudBlobClient()
+        {
+            return blobClient ?? GetStorageAccount().CreateCloudBlobClient();
+        }
+
+        private static string LoadConnectionStringFromConfigration()
+        {
+            return CloudConfigurationManager.GetSetting("StorageConnectionString");
+        }
+
+        private static CloudStorageAccount GetStorageAccount()
+        {
+            if (storageAccount == null)
+            {
+                var connectionString = LoadConnectionStringFromConfigration();
+                storageAccount = CloudStorageAccount.Parse(connectionString);
+            }
+
+            return storageAccount;
+        }
+
+    }
+}
